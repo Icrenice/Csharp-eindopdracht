07a579f8473ff64fa4505003cae21ebdf4abd7b1
icrenice <ericmensjunior@hotmail.com>
2025-03-13 11:56:32 +0100

    enclosure en zoo toegepast in models
    --by eric

--- Dierentuin/Models/Enclosure.cs
+++ Dierentuin/Models/Enclosure.cs
@@ -1,6 +1,135 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using System.Linq;             
+using System.Collections.Generic;
 namespace Dierentuin.Models;
 
 public class Enclosure
 {
+    // Primaire sleutel
+    public int Id { get; set; }
+
+    // Verplichte naam van het verblijf
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    // Klimaattype (bijv. Tropical, Temperate, Arctic)
+    public Klimaat Klimaat { get; set; }
+
+    // Type leefgebied (bijv. Bos, Water, Woestijn, Grasland).
+    public LeefgebiedType LeefgebiedType { get; set; }
+
+    // Beveiligingsniveau (Low, Medium, High) – evt. hernoemen naar "BeveiligingsNiveau"
+    public SecurityLevel SecurityLevel { get; set; }
+
+    // Oppervlakte in vierkante meters
+    public double Size { get; set; }
+
+    // Navigatie-eigenschap: lijst met dieren in dit verblijf
+    public List<Animal> Animals { get; set; } = new();
+
     
-}
+    /// Methode om aan te geven welke dieren wakker worden of gaan slapen bij zonsopgang.
+  
+    public void Zonsopgang()
+    {
+        foreach (var animal in Animals)
+        {
+            switch (animal.ActiviteitsPatroon)
+            {
+                case ActiviteitsPatroon.Dagactief:
+                    animal.IsWakker = true;
+                    break;
+                case ActiviteitsPatroon.Nachtdier:
+                    animal.IsWakker = false;
+                    break;
+                case ActiviteitsPatroon.Wisselend:
+                    animal.IsWakker = true;
+                    break;
+            }
+        }
+    }
+
+    
+    /// Methode om aan te geven welke dieren wakker worden of gaan slapen bij zonsondergang.
+
+    public void Zonsondergang()
+    {
+        foreach (var animal in Animals)
+        {
+            switch (animal.ActiviteitsPatroon)
+            {
+                case ActiviteitsPatroon.Dagactief:
+                    animal.IsWakker = false;
+                    break;
+                case ActiviteitsPatroon.Nachtdier:
+                    animal.IsWakker = true;
+                    break;
+                case ActiviteitsPatroon.Wisselend:
+                    animal.IsWakker = false;
+                    break;
+            }
+        }
+    }
+
+   
+    /// Methode om aan te geven welke dieren wat eten  .
+   
+
+    public void Voedertijd()
+    {
+        // Sorteer dieren van klein naar groot:
+        var orderedAnimals = Animals.OrderBy(a => a.Size).ToList();
+
+        for (int i = 0; i < orderedAnimals.Count; i++)
+        {
+            var carnivoor = orderedAnimals[i];
+            if (carnivoor.DietaryClass == DietaryClass.carnivoor) 
+            {
+                // Vind dieren die kleiner zijn:
+                var prooien = orderedAnimals
+                    .Where(p => p.Size < carnivoor.Size)
+                    .ToList();
+
+                // Haal deze 'prooien' uit het verblijf
+                foreach (var prooi in prooien)
+                {
+                    Animals.Remove(prooi);
+                }
+            }
+           
+        }
+    }
+
+    
+    /// Methode om te controleren of alle voorwaarden in dit verblijf kloppen:
+    /// - Is er voldoende ruimte?
+    /// - Voldoet het beveiligingsniveau aan de eisen van elk dier?
+  
+    /// <returns>Lijst met foutmeldingen (indien er problemen zijn).</returns>
+    public List<string> CheckConstraints()
+    {
+        var fouten = new List<string>();
+
+        // Check totale benodigde ruimte:
+        double totaleBenodigdeRuimte = Animals.Sum(a => a.SpaceRequirement);
+        if (totaleBenodigdeRuimte > Size)
+        {
+            fouten.Add($"Te weinig ruimte in '{Name}': " +
+                       $"Nodig is {totaleBenodigdeRuimte} m², maar beschikbaar is {Size} m².");
+        }
+
+        // Check beveiliging:
+        foreach (var animal in Animals)
+        {
+            if (animal.SecurityRequirement > SecurityLevel)
+            {
+                fouten.Add($"Beveiligingsniveau te laag voor dier '{animal.Name}' " +
+                           $"(Benodigd: {animal.SecurityRequirement}, Huidig: {SecurityLevel}).");
+            }
+        }
+
+    
+        return fouten;
+    }
--- Dierentuin/Models/Zoo.cs
+++ Dierentuin/Models/Zoo.cs
@@ -2,5 +2,115 @@ namespace Dierentuin.Models;
 
 public class Zoo
 {
+    // Primaire sleutel
+    public int Id { get; set; }
+
+    // Naam van de dierentuin
+    [Required]
+    public string Naam { get; set; } = "Mijn Dierentuin";
+
+    // Alle verblijven in deze dierentuin
+    public List<Enclosure> Verblijven { get; set; } = new();
+
+    // Lijst met alledieren
+    public List<Animal> AlleDieren { get; set; } = new();
+
+
+    //Zonsopgang roep de Zonsopgang-methode aan voor elk verblijf dieren bepalen of ze opstaan  of slapen
+    public void Zonsopgang()
+    {
+        foreach (var verblijf in Verblijven)
+        {
+            verblijf.Zonsopgang();
+        }
+    }
+
+    // Zonsondergang roep de Zonsondergang-methode aan voor elk verblijf dieren bepalen of  ze opstaan of slapen
+   
+    public void Zonsondergang()
+    {
+        foreach (var verblijf in Verblijven)
+        {
+            verblijf.Zonsondergang();
+        }
+    }
+
     
+    // Voedertijd: Roep de Voedertijd-methode aan voor elk verblijf 
+    public void Voedertijd()
+    {
+        foreach (var verblijf in Verblijven)
+        {
+            verblijf.Voedertijd();
+        }
+    }
+
+
+    // CheckConstraints: controleer voor elk verblijf
+    // of aan de eisen is voldaan (ruimte, beveiliging, etc.)
+    // Keer terug met een lijst van meldingen.
+  
+    // <returns>Lijst met foutmeldingen</returns>
+    public List<string> CheckConstraints()
+    {
+        var fouten = new List<string>();
+
+        foreach (var verblijf in Verblijven)
+        {
+            var verblijfFouten = verblijf.CheckConstraints();
+            if (verblijfFouten.Any())
+            {
+                // Je kunt prefixen met de naam van het verblijf
+                foreach (var fout in verblijfFouten)
+                {
+                    fouten.Add($"[{verblijf.Name}] {fout}");
+                }
+            }
+        }
+
+        return fouten;
+    }
+
+    // AutoAssign: automatisch dieren indelen in verblijven.
+    // removeExisting = true => verwijder alle verblijven + indeling
+    // en maak een nieuwe indeling.
+    // removeExisting = false => gebruik bestaande verblijven en vul niet toegewezen dieren aan.
+    public void AutoAssign(bool removeExisting = false)
+    {
+        if (removeExisting)
+        {
+            // Extreem voorbeeld: verwijder alle verblijven
+            Verblijven.Clear();
+            // Maak hier eventueel nieuwe verblijven aan:
+            Verblijven.Add(new Enclosure
+            {
+                Name = "Nieuw Verblijf 1",
+                Size = 100,
+                SecurityLevel = SecurityLevel.Medium
+            });
+            Verblijven.Add(new Enclosure
+            {
+                Name = "Nieuw Verblijf 2",
+                Size = 200,
+                SecurityLevel = SecurityLevel.High
+            });
+        }
+
+       
+        var unassignedAnimals = AlleDieren.Where(a => a.EnclosureId == null).ToList();
+
+        foreach (var dier in unassignedAnimals)
+        {
+
+            var suitableEnclosure = Verblijven.FirstOrDefault();
+            if (suitableEnclosure != null)
+            {
+                suitableEnclosure.Animals.Add(dier);
+                dier.EnclosureId = suitableEnclosure.Id;
+            }
+        }
+        
+
+    }
+}
 }
